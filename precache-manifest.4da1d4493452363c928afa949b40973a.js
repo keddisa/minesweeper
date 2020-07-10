0e9b048f0272c0c346f9bb1f244107a83ee88c61
keddisa <keddisa@gmail.com>
2020-07-10 16:33:29 -0400

    Updates

---
+++ precache-manifest.4da1d4493452363c928afa949b40973a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d22635a9695e7b9e1e5e63c32d218ef",
+    "url": "/minesweeper/index.html"
+  },
+  {
+    "revision": "5d0fd250860b77c88276",
+    "url": "/minesweeper/static/css/main.1fe6e4cc.chunk.css"
+  },
+  {
+    "revision": "409b554868fb2ba10f0b",
+    "url": "/minesweeper/static/js/2.2ed9827b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/minesweeper/static/js/2.2ed9827b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d0fd250860b77c88276",
+    "url": "/minesweeper/static/js/main.229ec00c.chunk.js"
+  },
+  {
+    "revision": "4d95e40b2d19d90a20c3",
+    "url": "/minesweeper/static/js/runtime-main.d1df0494.js"
+  },
+  {
+    "revision": "d968deced8d8e8669a339f1913bea641",
+    "url": "/minesweeper/static/media/NewPatagonia-Normal.d968dece.otf"
+  },
+  {
+    "revision": "96328ceeab338bee0c957ed3d02cd818",
+    "url": "/minesweeper/static/media/NewPatagonia-Regular.96328cee.otf"
+  }
+]);
